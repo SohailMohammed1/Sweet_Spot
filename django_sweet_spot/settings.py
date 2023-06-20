187c5d14dcd63fd9a328de818f9e249e8cb8020f
SohailMohammed1 <sohailm2233@gmail.com>
2023-06-20 17:24:20 +0000

    connected elephantSQL to settings.py

--- django_sweet_spot/settings.py
+++ django_sweet_spot/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('u0l6nO8kR-Z-OsRkSg-FAFylmVKPfIYM')
+SECRET_KEY = 'u0l6nO8kR-Z-OsRkSg-FAFylmVKPfIYM'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +81,16 @@ WSGI_APPLICATION = 'django_sweet_spot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+ # DATABASES = {
+ #     'default': {
+ #         'ENGINE': 'django.db.backends.sqlite3',
+ #         'NAME': BASE_DIR / 'db.sqlite3',
+ #     }
+ # }
+
+
 DATABASES = {
-    'default': dj_database_url(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse("postgres://jybmazpw:u0l6nO8kR-Z-OsRkSg-FAFylmVKPfIYM@lucky.db.elephantsql.com/jybmazpw")
 }
 
 
